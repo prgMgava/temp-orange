93647c5b7d6f85ed923734d755270a1a65309568
prgMgava <78923115+prgMgava@users.noreply.github.com>
2024-01-23 12:43:23 -0300

    feat: add web instace page

--- src/layouts/web-instance/index.jsx
+++ src/layouts/web-instance/index.jsx
@@ -13,7 +13,10 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 // @mui material components
+import { Icon, Menu, MenuItem } from "@mui/material";
 import Card from "@mui/material/Card";
+
+import { useState } from "react";
 
 // Data
 // Orange APi examples
@@ -23,25 +26,114 @@ import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 
 // Orange APi components
 import SoftBox from "components/SoftBox";
+import SoftButton from "components/SoftButton";
+import SoftTypography from "components/SoftTypography";
 
 function WebInstance() {
+  const [menu, setMenu] = useState(null);
+
+  const openMenu = ({ currentTarget }) => setMenu(currentTarget);
+  const closeMenu = () => setMenu(null);
+
   return (
     <DashboardLayout>
       <DashboardNavbar />
 
       <SoftBox py={3}>
-        <SoftBox mb={3}>
-          <Card>
-            <SoftBox
-              sx={{
-                "& .MuiTableRow-root:not(:last-child)": {
-                  "& td": {
-                    borderBottom: ({ borders: { borderWidth, borderColor } }) =>
-                      `${borderWidth[1]} solid ${borderColor}`,
-                  },
-                },
+        <SoftBox mb={3} pt={2} px={2}>
+          <Card sx={{ padding: "16px", display: "flex", flexDirection: "row" }}>
+            <div style={{ flex: 1 }}>
+              <SoftBox>
+                <SoftTypography fontWeight="bold" variant="h6" noWrap>
+                  Realize o pagamento
+                </SoftTypography>
+                <SoftTypography fontWeight="normal" variant="caption">
+                  Para utilizar essa instância é necessário realizar o pagamento
+                  da mesma. O valor é de R$ 99,00
+                </SoftTypography>
+              </SoftBox>
+              <SoftBox mt="16px">
+                <SoftTypography fontWeight="bold" variant="h6" noWrap>
+                  Assinatura
+                </SoftTypography>
+                <SoftBox>
+                  <SoftTypography fontWeight="bold" variant="caption">
+                    Status atual:{" "}
+                  </SoftTypography>
+                  <SoftTypography
+                    fontWeight="bold"
+                    variant="button"
+                    color="error"
+                  >
+                    PENDENTE ASSINATURA
+                  </SoftTypography>
+                </SoftBox>
+                <SoftBox>
+                  <SoftTypography fontWeight="normal" variant="caption">
+                    <SoftTypography fontWeight="bold" variant="caption">
+                      Expira em:{" "}
+                    </SoftTypography>
+                    <SoftTypography fontWeight="normal" variant="caption">
+                      Expirado!
+                    </SoftTypography>
+                  </SoftTypography>
+                </SoftBox>
+              </SoftBox>
+              <SoftBox mt="8px">
+                <SoftButton color={"info"}>Assinar</SoftButton>
+              </SoftBox>
+            </div>
+            <SoftBox color="text" px={2}>
+              <Icon
+                sx={{ cursor: "pointer", fontWeight: "bold" }}
+                fontSize="small"
+                onClick={openMenu}
+              >
+                more_vert
+              </Icon>
+            </SoftBox>
+            <Menu
+              id="simple-menu"
+              anchorEl={menu}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "left",
               }}
-            ></SoftBox>
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(menu)}
+              onClose={closeMenu}
+            >
+              <MenuItem onClick={closeMenu}>
+                {" "}
+                <SoftBox color="text" px={2} display="flex">
+                  <Icon sx={{ cursor: "pointer" }} fontSize="small">
+                    edit
+                  </Icon>
+                </SoftBox>
+                Editar
+              </MenuItem>
+              <MenuItem onClick={closeMenu}>
+                {" "}
+                <SoftBox color="text" px={2} display="flex">
+                  <Icon sx={{ cursor: "pointer" }} fontSize="small">
+                    credit_card
+                  </Icon>
+                </SoftBox>
+                Pagamentos
+              </MenuItem>
+              <MenuItem onClick={closeMenu}>
+                {" "}
+                <SoftBox color="text" px={2} display="flex">
+                  <Icon sx={{ cursor: "pointer" }} fontSize="small">
+                    delete_outlined
+                  </Icon>
+                </SoftBox>
+                Excluir
+              </MenuItem>
+            </Menu>
           </Card>
         </SoftBox>
       </SoftBox>
